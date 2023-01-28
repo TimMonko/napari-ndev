6d105a1903ac06ffe9529bad052b3739605722cb
TimMonko <47310455+TimMonko@users.noreply.github.com>
2023-01-27 18:04:43 -0600

    batch annotator and test widget

--- src/napari_ndev/_widget.py
+++ src/napari_ndev/_widget.py
@@ -1,12 +1,8 @@
 """
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
+neural development (nDev) widget collection
 """
 import pathlib
+import string
 from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
@@ -19,7 +15,7 @@ if TYPE_CHECKING:
 @magic_factory(
     auto_call=False,
     result_widget=True,
-    call_button="Create Output Folders",
+    call_button="Save Layers to Output Folders",
     file_directory=dict(
         widget_type="FileEdit", mode="d", label="File Directory"
     ),
@@ -31,14 +27,47 @@ def batch_annotator(
     file_directory=pathlib.Path(),
     output_folder_prefix="Annotated",
 ):
+    """Batch Annotation
+
+    Used for annotating images and saving images of interest into a folder
+      for the image and a folder for the labels.
+    The GUI allows selecting of the intended label layer and image layer,
+      as well as the prefix for the output folders.
+    This output folder can already exist: mkdir(exist_ok=True),
+      so should be used to save multiple images within the same folder group.
+    The images are saved as .tif files though this can be adjusted in
+      the saver function, and could be added as a GUI element.
+    napari.layers. is event connected, so labels and image selection
+      will update as new labels and images are added.
+    """
+
     def saver(image_type, folder_suffix, save_suffix=".tif"):
         save_folder = file_directory / str(
             output_folder_prefix + folder_suffix
         )
         save_folder.mkdir(parents=False, exist_ok=True)
-        save_path = save_folder / str(image.name + save_suffix)
+
+        # convert AICSImageIO (and others, potentially)
+        # tile opened image layer names.
+        # Otherwise these are invalid file names
+        def format_filename(char_string):
+            valid_chars = "-_.() {}{}".format(
+                string.ascii_letters, string.digits
+            )  # valid character list
+            filename = "".join(
+                char for char in char_string if char in valid_chars
+            )  # join only valid characters from input string
+            filename = filename.replace(
+                " ", "_"
+            )  # replace spaces with underscores
+            return filename
+
+        save_name = format_filename(
+            str(image.name + folder_suffix + save_suffix)
+        )
+        save_path = save_folder / save_name  # path-like
         image_type.save(path=str(save_path))
 
     saver(image, "_images", ".tif")
     saver(labels, "_labels", ".tif")
-    return
+    return "success"
