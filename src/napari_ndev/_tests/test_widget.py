299d2c25ab9ef9e830055de8e18b21951271404b
TimMonko <47310455+TimMonko@users.noreply.github.com>
2023-01-27 18:04:43 -0600

    batch annotator and test widget

---
+++ src/napari_ndev/_tests/test_widget.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from napari_ndev import batch_annotator
+
+
+# make_napari_viewer is a pytest fixture that returns a napari viewer object
+# capsys is a pytest fixture that captures stdout and stderr output streams
+def test_batch_annotator(make_napari_viewer, capsys):
+    # make viewer and add an image layer using our fixture
+    viewer = make_napari_viewer()
+    viewer.add_image(np.random.random((100, 100)))
+    viewer.add_labels(np.random.random((100, 100)))
+
+    # create our widget, passing in the viewer
+    my_widget = batch_annotator(viewer)
+
+    # call our widget method
+    my_widget._on_click()
+
+    # read captured output and check that it's as we expected
+    captured = capsys.readouterr()
+    assert captured.out == "napari has 1 layers\n"
