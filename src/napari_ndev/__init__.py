b6bc410bc7213db265cb0c54198b1fba27a656dc
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-01-18 16:11:37 -0600

    add custom apoc feature string generator
    -adds widget to generate feature string
    -fixes custom feature ability in apoc training/predict widget

--- src/napari_ndev/__init__.py
+++ src/napari_ndev/__init__.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._apoc_container import SegmentImg
+from ._apoc_container import CustomApoc, SegmentImg
 from ._napari_utilities import annotation_saver, rescale_by
 from ._utilities_container import MetaImg
 from ._widget import (
@@ -22,4 +22,5 @@ __all__ = [
     "rescale_by",
     "MetaImg",
     "SegmentImg",
+    "CustomApoc",
 ]
