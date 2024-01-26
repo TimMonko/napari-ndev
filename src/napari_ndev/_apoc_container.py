c9cae8cd98e84986c479847719780cfe87c9014b
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-01-26 17:16:16 -0600

    update for tests

--- src/napari_ndev/_apoc_container.py
+++ src/napari_ndev/_apoc_container.py
@@ -28,7 +28,6 @@ from magicgui.widgets import (
     create_widget,
 )
 from napari import layers
-from napari.viewer import current_viewer
 
 if TYPE_CHECKING:
     import napari
@@ -134,9 +133,7 @@ class SegmentImg(Container):
 
         def current_layers(_):
             return [
-                x
-                for x in current_viewer().layers
-                if isinstance(x, layers.Image)
+                x for x in self._viewer.layers if isinstance(x, layers.Image)
             ]
 
         self._image_layer = Select(
