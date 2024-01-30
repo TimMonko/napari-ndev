d503726b1f94471f05380c61b9f7cb31ba8c1315
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-01-30 16:23:13 -0600

    tests update

--- src/napari_ndev/_tests/test_apoc_container.py
+++ src/napari_ndev/_tests/test_apoc_container.py
@@ -1,38 +1,9 @@
 import os
 import tempfile
 
-import numpy as np
-
-from napari_ndev import ApocContainer
 import pytest
 
-image = np.asarray(
-    [
-        [0, 0, 1, 1],
-        [0, 0, 1, 1],
-        [2, 2, 1, 1],
-        [2, 2, 1, 1],
-    ]
-)
-
-annotation = np.asarray(
-    [[0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0]]
-)
-
-# test the training on each filter!!!
-
-# def test_update_metadata(make_napari_viewer):
-#     """
-#     Test the _update_channel_order method of the SegmentImg class.
-#     """
-#     wdg = SegmentImg(make_napari_viewer())
-#     with tempfile.TemporaryDirectory() as tmpdir:
-#         # Create dummy image file
-#         image_path = os.path.join(tmpdir, "dummy_image.tif")
-#         with open(image_path, "w") as f:
-#             f.write("dummy image content")
-#         wdg._image_directory.value = tmpdir
-#         assert wdg._image_channels.choices == ["Channel_0"]
+from napari_ndev import ApocContainer
 
 
 def test_update_channel_order(make_napari_viewer):
@@ -83,4 +54,3 @@ def test_update_classifier_metadata(make_napari_viewer, dummy_classifier_file):
     assert wdg._max_depth.value == 5
     assert wdg._num_trees.value == 100
     assert wdg._positive_class_id.value == 2
-
