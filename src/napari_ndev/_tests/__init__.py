5ae171296a82c68466a41182c89923f8d7deb1b4
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-02-03 13:10:37 -0600

    remove test exceptions, just notox now

--- src/napari_ndev/_tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-POSIX = os.name == "posix"
-WINDOWS = os.name == "nt"
-LINUX = sys.platform.startswith("linux")
-MACOS = sys.platform.startswith("darwin")
-CI = os.getenv("CI")
