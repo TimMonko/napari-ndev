42bc3e577978353ba2a5f3e03a14319f217eee67
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-02-03 12:19:16 -0600

    Update test_apoc_container.py

--- src/napari_ndev/_tests/test_apoc_container.py
+++ src/napari_ndev/_tests/test_apoc_container.py
@@ -180,24 +180,20 @@ def test_image_predict(make_napari_viewer, test_data, trained_classifier_file):
 )
 def test_batch_predict_normal_operation(make_napari_viewer, tmp_path):
     image_directory = pathlib.Path(
-        os.path.join(
-            "src", "napari_ndev", "_tests", "resources", "Apoc", "Images"
-        )
+        "src", "napari_ndev", "_tests", "resources", "Apoc", "Images"
     )
     num_files = len(list(image_directory.glob("*.tif")))
     output_directory = tmp_path / "output"
     output_directory.mkdir()
 
     classifier = pathlib.Path(
-        os.path.join(
-            "src",
-            "napari_ndev",
-            "_tests",
-            "resources",
-            "Apoc",
-            "Classifiers",
-            "newlabels_pixel_classifier.cl",
-        )
+        "src",
+        "napari_ndev",
+        "_tests",
+        "resources",
+        "Apoc",
+        "Classifiers",
+        "newlabels_pixel_classifier.cl",
         # r"C:\Users\timmo\napari-ndev\src\napari_ndev\_tests\resources"
         # r"\Apoc\Classifiers\newlabels_pixel_classifier.cl"
     )
@@ -221,9 +217,7 @@ def test_batch_predict_normal_operation(make_napari_viewer, tmp_path):
 )
 def test_batch_predict_exception_logging(make_napari_viewer, tmp_path):
     image_directory = pathlib.Path(
-        os.path.join(
-            "src", "napari_ndev", "_tests", "resources", "Apoc", "Images"
-        )
+        "src", "napari_ndev", "_tests", "resources", "Apoc", "Images"
     )
     num_files = len(list(image_directory.glob("*.tif")))
     output_directory = tmp_path / "output"
