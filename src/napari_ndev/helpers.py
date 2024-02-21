0795fb745780fc6401df7c56fc0100a5f96befcd
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-02-21 10:48:57 -0600

    update helpers.py to have a list of formats
    Default list of formats should represent common microscope formats but I wasn't too extensive. This can easily be updated in the future. I did attempt to use `aicsimageio.formats.FORMAT_IMPLEMENTATIONS` but this includes file types that I don't want to search for, like `txt`

--- src/napari_ndev/helpers.py
+++ src/napari_ndev/helpers.py
@@ -3,25 +3,48 @@ import time
 from pathlib import Path
 from typing import List, Tuple, Union
 
-PathLike = Union[str, Path]
-
 
 def get_directory_and_files(
-    dir: PathLike, pattern: str = "*"
+    dir: Union[str, Path],
+    pattern: Union[List[str], str] = [
+        "tif",
+        "tiff",
+        "nd2",
+        "czi",
+        "lif",
+        "oib",
+        "png",
+        "jpg",
+        "jpeg",
+        "bmp",
+        "gif",
+    ],
 ) -> Tuple[Path, List[Path]]:
     """
     Get the directory and files in the specified directory.
 
     Args:
-        dir (PathLike): The directory path.
-        pattern (str, optional): The file pattern to match. Defaults to '*'.
+        dir (Union[str, Path]): The directory path.
+        pattern (Union[List[str], str], optional): The file pattern(s) to match
+            If a string is provided, it will be treated as a single pattern.
+            If a list is provided, each element will be treated as a separate
+            pattern.
+            Defaults to ['tif', 'tiff', 'nd2', 'czi', 'lif',
+            'oib', 'png', 'jpg', 'jpeg', 'bmp', 'gif']
 
     Returns:
         Tuple[Path, List[Path]]: A tuple containing the directory path and a
         list of file paths.
     """
     directory = Path(dir)
-    files = list(directory.glob(pattern))
+
+    pattern = [pattern] if isinstance(pattern, str) else pattern
+    pattern_glob = [f"*.{pat}" for pat in pattern]
+
+    files = []
+    for p_glob in pattern_glob:
+        for file in directory.glob(p_glob):
+            files.append(file)
     return directory, files
 
 
@@ -51,7 +74,7 @@ def check_for_missing_files(
     return missing_files
 
 
-def setup_logger(log_loc=PathLike):
+def setup_logger(log_loc=Union[str, Path]):
     """
     Set up a logger with the specified log location.
 
