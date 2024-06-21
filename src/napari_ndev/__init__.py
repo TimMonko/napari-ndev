4aeb90665d3bc910708f11d41495823949679028
Tim Monko <47310455+TimMonko@users.noreply.github.com>
2024-06-21 13:29:36 -0500

    remove direct access to helpers

--- src/napari_ndev/__init__.py
+++ src/napari_ndev/__init__.py
@@ -9,13 +9,13 @@ from ._plate_mapper import PlateMapper
 from ._rescale_by import RescaleBy
 from ._utilities_container import UtilitiesContainer
 from ._workflow_container import WorkflowContainer
-from .helpers import (
-    check_for_missing_files,
-    get_channel_names,
-    get_directory_and_files,
-    get_squeezed_dim_order,
-    setup_logger,
-)
+# from .helpers import (
+#     check_for_missing_files,
+#     get_channel_names,
+#     get_directory_and_files,
+#     get_squeezed_dim_order,
+#     setup_logger,
+# )
 
 __all__ = [
     "WorkflowContainer",
@@ -24,9 +24,9 @@ __all__ = [
     "ApocFeatureStack",
     "RescaleBy",
     "PlateMapper",
-    "get_directory_and_files",
-    "check_for_missing_files",
-    "setup_logger",
-    "get_squeezed_dim_order",
-    "get_channel_names",
+    # "get_directory_and_files",
+    # "check_for_missing_files",
+    # "setup_logger",
+    # "get_squeezed_dim_order",
+    # "get_channel_names",
 ]
