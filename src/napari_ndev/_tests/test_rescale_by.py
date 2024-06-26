df1f9745f467afbc5ba580adc488cf4f096c193c
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-06-25 23:15:32 -0500

    Update test_rescale_by.py

--- src/napari_ndev/_tests/test_rescale_by.py
+++ src/napari_ndev/_tests/test_rescale_by.py
@@ -7,24 +7,18 @@ image_4d = np.random.random((1, 1, 10, 10))
 
 
 @pytest.mark.parametrize(
-    "scale_x, scale_y, scale_z, scale_in_z, expected_scale",
+    "scale_tuple, expected_scale",
     [
-        (1.5, 2.5, 3.5, False, (1, 1, 2.5, 1.5)),
-        (1.5, 2.5, 3.5, True, (1, 3.5, 2.5, 1.5)),
+        ((1.5, 2.5, 3.5), (1, 1.5, 2.5, 3.5)),
     ],
 )
-def test_rescale_by(
-    make_napari_viewer, scale_x, scale_y, scale_z, scale_in_z, expected_scale
-):
+def test_rescale_by(make_napari_viewer, scale_tuple, expected_scale):
     viewer = make_napari_viewer()
     viewer.add_image(image_4d)
     container = RescaleBy(viewer)
 
     container._layer_to_scale.value = viewer.layers["image_4d"]
-    container._scale_x.value = scale_x
-    container._scale_y.value = scale_y
-    container._scale_z.value = scale_z
-    container._scale_in_z.value = scale_in_z
+    container._scale_tuple.value = scale_tuple
 
     container.rescale_by()
 
@@ -41,6 +35,4 @@ def test_inherit_from(make_napari_viewer):
     container._inherit_from_layer.value = viewer.layers["image_4d"]
 
     # Check if the scale values have been inherited correctly
-    assert container._scale_x.value == 1.5
-    assert container._scale_y.value == 2.5
-    assert container._scale_z.value == 3.5
+    assert container._scale_tuple.value == (3.5, 2.5, 1.5)
