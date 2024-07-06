e689fd3136c9099d528405ae9b51f82c0572ffa8
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-07-06 00:04:48 -0500

    fix up plot showing and mem management

--- src/napari_ndev/_image_overview.py
+++ src/napari_ndev/_image_overview.py
@@ -1,23 +1,36 @@
+import matplotlib.pyplot as plt
+
+
 class ImageOverview:
-    def __init__(self, image_sets):
+    def __init__(
+        self,
+        image_sets: dict,
+        xscale: float = 3,
+        yscale: float = 3,
+        show: bool = False,
+    ):
         self.image_sets = image_sets
+        self.xscale = xscale
+        self.yscale = yscale
+        plt.ioff()
         self.fig, self.axs = self._construct_overview()
+        if show:
+            plt.show()
+        plt.close()
 
     def _construct_overview(self):
         import stackview
-        import matplotlib.pyplot as plt
-        from numpy import zeros
 
+        # plt.ioff()
         # create the subplot grid
         num_rows = len(self.image_sets)
         num_columns = max(
-            [len(image_set['image']) for image_set in self.image_sets]
+            [len(image_set["image"]) for image_set in self.image_sets]
         )
-
         fig, axs = plt.subplots(
             num_rows,
             num_columns,
-            figsize=(num_columns * 5, num_rows * 5)
+            figsize=(num_columns * self.xscale, num_rows * self.yscale),
         )
 
         if num_rows == 1:
@@ -29,30 +42,35 @@ class ImageOverview:
         for row, image_set in enumerate(self.image_sets):
             # print(image_set)
             # for col, image in enumerate(image_set):
-            for col in range(len(image_set['image'])):
+            for col in range(len(image_set["image"])):
                 # create a dictionary from the col-th values of each key
                 image_dict = {
                     key: value[col] for key, value in image_set.items()
                 }
 
-                if image_dict['image'] is None:
-                    image_dict['image'] = zeros((1, 1))
+                # turn off the subplot and continue if there is no image
+                if image_dict.get("image") is None:
+                    axs[row][col].axis("off")
+                    continue
 
                 # create a labels key if it doesn't exist, but does in colormap
-                cmap = image_dict.get('colormap')
-                if cmap is not None and cmap.lower() == 'labels':
-                    image_dict['labels'] = True
+                cmap = image_dict.get("colormap")
+                if cmap is not None and cmap.lower() == "labels":
+                    image_dict["labels"] = True
 
                 stackview.imshow(**image_dict, plot=axs[row][col])
 
         plt.subplots_adjust(wspace=0.1, hspace=0.1)
+
         return fig, axs
 
-    def show(self):
-        self.fig.show()
-
-    def save(self, directory: str = None, filename: str = None, ):
+    def save(
+        self,
+        directory: str = None,
+        filename: str = None,
+    ):
         import pathlib
+
         dir = pathlib.Path(directory)
         dir.mkdir(parents=True, exist_ok=True)
         filepath = dir / filename
