21dd6334d30ede0f360c66d88953cc74cc1ecb7b
Tim Monko <47310455+TimMonko@users.noreply.github.com>
2024-08-26 17:40:00 -0500

    creation of measure container
    primary layout of widgets and collection of images and channels

--- src/napari_ndev/__init__.py
+++ src/napari_ndev/__init__.py
@@ -10,12 +10,14 @@ from ._plate_mapper import PlateMapper
 from ._utilities_container import UtilitiesContainer
 from ._workflow_container import WorkflowContainer
 from .image_overview import ImageOverview, image_overview
+from ._measure_container import MeasureContainer
 
 __all__ = [
     "WorkflowContainer",
     "UtilitiesContainer",
     "ApocContainer",
     "ApocFeatureStack",
+    "MeasureContainer",
     "PlateMapper",
     "ImageOverview",
     "image_overview",
