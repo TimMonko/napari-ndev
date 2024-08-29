f11dd05c2bc1a8e1c7c635e9e1601431c559e9bc
Tim Monko <47310455+TimMonko@users.noreply.github.com>
2024-08-29 15:15:29 -0500

    first complete measure and tests

--- src/napari_ndev/_tests/test_measure.py
+++ src/napari_ndev/_tests/test_measure.py
@@ -1,8 +1,12 @@
 import numpy as np
+import pandas as pd
 import pytest
 from typing import Union, List
 from bioio_base.types import ArrayLike
-from napari_ndev.measure import _generate_measure_dict, _convert_to_list
+from napari_ndev.measure import (
+    _generate_measure_dict, _convert_to_list, _extract_info_from_id_string,
+    measure_regionprops
+)
 
 @pytest.mark.parametrize(
     "arg, expected",
@@ -81,3 +85,57 @@ def test_generate_measure_dict(label_images, label_names, intensity_images, inte
     else:
         assert result['intensity_images'] is None
         assert result['intensity_names'] is None
+        
+        
+def test_extract_info_from_id_string():
+    id_string = "P14-A6__2024-07-16 25x 18HIC ncoa4 FT dapi obl 01"
+    id_regex = {
+        'scene': r'(P\d{1,3}-\w+)__',
+        'well': r'-(\w+)__',
+        'HIC': r'(\d{1,3})HIC',
+        'exp': r'obl (\d{2,3})'
+    }
+    expected = {'scene': 'P14-A6', 'well': 'A6', 'HIC': '18', 'exp': '01'}
+    result = _extract_info_from_id_string(id_string, id_regex)
+    assert result == expected
+
+# Define test data
+label_image_2d = np.array([[0, 0, 1], [0, 1, 1], [1, 1, 0]])
+label_image_3d = np.array([[[0, 0, 1], [0, 1, 1], [1, 1, 0]], [[0, 0, 1], [0, 1, 1], [1, 1, 0]]])
+intensity_image_2d = np.array([[10, 10, 20], [10, 20, 20], [20, 20, 10]])
+intensity_image_3d = np.array([[[10, 10, 20], [10, 20, 20], [20, 20, 10]], [[10, 10, 20], [10, 20, 20], [20, 20, 10]]])
+id_string = 'P14-A6__2024-07-16 25x 18HIC ncoa4 FT dapi obl 01'
+id_regex = {
+    'well': '-(\w+)__',
+    'HIC': r'(\d{1,3})HIC',
+    'exp': r'obl (\d{2,3})'
+}
+
+
+@pytest.mark.parametrize(
+    "label_images, label_names, intensity_images, intensity_names, properties, scale, id_string, id_regex, save_data_path, expected_columns",
+    [
+        (label_image_2d, None, None, None, ['area'], (1.0, 1.0), id_string, id_regex, None, ['id_string', 'well', 'HIC', 'exp', 'area']),
+        ([label_image_2d], None, [intensity_image_2d], None, ['area', 'intensity_mean'], (1.0, 1.0), "test_id", None, None, ['id_string', 'area', 'intensity_mean']),
+        ([label_image_3d], None, None, None, ['area'], (1.0, 1.0, 1.0), "test_id", None, None, ['id_string', 'area']),
+        ([label_image_3d], None, [intensity_image_3d], None, ['area', 'intensity_mean'], (1.0, 1.0, 1.0), "test_id", None, None, ['id_string', 'area', 'intensity_mean']),
+    ]
+)
+def test_measure_regionprops(label_images, label_names, intensity_images, intensity_names, properties, scale, id_string, id_regex, save_data_path, expected_columns):
+    result_df = measure_regionprops(
+        label_images=label_images,
+        label_names=label_names,
+        intensity_images=intensity_images,
+        intensity_names=intensity_names,
+        properties=properties,
+        scale=scale,
+        id_string=id_string,
+        id_regex=id_regex,
+        save_data_path=save_data_path,
+    )
+    
+    assert isinstance(result_df, pd.DataFrame)
+    # Check if the DataFrame contains the expected columns
+    assert all(column in result_df.columns for column in expected_columns)
+    # Check if the id_string column contains the correct value
+    assert (result_df['id_string'] == id_string).all()
