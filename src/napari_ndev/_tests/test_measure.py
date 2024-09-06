7b299afdc1b4f47719d4d883539eef16cbec2e2d
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-09-06 09:37:18 -0500

    fix measure file formatting

--- src/napari_ndev/_tests/test_measure.py
+++ src/napari_ndev/_tests/test_measure.py
@@ -1,12 +1,18 @@
+from typing import List, Union
+
 import numpy as np
 import pandas as pd
 import pytest
-from typing import Union, List
 from bioio_base.types import ArrayLike
+
 from napari_ndev.measure import (
-    _generate_measure_dict, _convert_to_list, _extract_info_from_id_string,
-    map_tx_dict_to_df_id_col, measure_regionprops
+    _convert_to_list,
+    _extract_info_from_id_string,
+    _generate_measure_dict,
+    map_tx_dict_to_df_id_col,
+    measure_regionprops,
 )
+
 
 @pytest.mark.parametrize(
     "arg, expected",
@@ -16,10 +22,15 @@ from napari_ndev.measure import (
         ("string", ["string"]),
         ([1, 2, 3], [1, 2, 3]),
         (np.array([1, 2, 3]), [np.array([1, 2, 3])]),
-        ([np.array([1, 2]), np.array([3, 4])], [np.array([1, 2]), np.array([3, 4])]),
-    ]
+        (
+            [np.array([1, 2]), np.array([3, 4])],
+            [np.array([1, 2]), np.array([3, 4])],
+        ),
+    ],
 )
-def test_convert_to_list(arg: Union[List, ArrayLike, str, None], expected: Union[List, None]):
+def test_convert_to_list(
+    arg: Union[List, ArrayLike, str, None], expected: Union[List, None]
+):
     result = _convert_to_list(arg)
     if isinstance(result, list):
         for res, exp in zip(result, expected):
@@ -38,64 +49,102 @@ intensity1 = np.array([[5, 6], [7, 8]])
 intensity2 = np.array([[9, 10], [11, 12]])
 intensity_list = [intensity1, intensity2]
 
+
 @pytest.mark.parametrize(
     "label_images, label_names, intensity_images, intensity_names, expected",
     [
-        (label1, None, None, None, {
-            'label_images': label1,
-            'label_names': ['label_0'],
-            'intensity_images': None,
-            'intensity_names': None
-        }),
-        (label1, None, intensity1, None, {
-            'label_images': label1,
-            'label_names': ['label_0'],
-            'intensity_images': intensity1,
-            'intensity_names': ['intensity_0']
-        }),
-        (label1, "custom_label", intensity1, "custom_intensity", {
-            'label_images': label1,
-            'label_names': ["custom_label"],
-            'intensity_images': intensity1,
-            'intensity_names': ["custom_intensity"]
-        }),
-        (label1, "custom_label", None, None, {
-            'label_images': label1,
-            'label_names': ["custom_label"],
-            'intensity_images': None,
-            'intensity_names': None
-        }),
-        (label_list, ["label_c1", "label_c2"], intensity_list, ["intensity_c1", "intensity_c2"], {
-            'label_images': label_list,
-            'label_names': ["label_c1", "label_c2"],
-            'intensity_images': intensity_list,
-            'intensity_names': ["intensity_c1", "intensity_c2"]
-        }),
-    ]
+        (
+            label1,
+            None,
+            None,
+            None,
+            {
+                "label_images": label1,
+                "label_names": ["label_0"],
+                "intensity_images": None,
+                "intensity_names": None,
+            },
+        ),
+        (
+            label1,
+            None,
+            intensity1,
+            None,
+            {
+                "label_images": label1,
+                "label_names": ["label_0"],
+                "intensity_images": intensity1,
+                "intensity_names": ["intensity_0"],
+            },
+        ),
+        (
+            label1,
+            "custom_label",
+            intensity1,
+            "custom_intensity",
+            {
+                "label_images": label1,
+                "label_names": ["custom_label"],
+                "intensity_images": intensity1,
+                "intensity_names": ["custom_intensity"],
+            },
+        ),
+        (
+            label1,
+            "custom_label",
+            None,
+            None,
+            {
+                "label_images": label1,
+                "label_names": ["custom_label"],
+                "intensity_images": None,
+                "intensity_names": None,
+            },
+        ),
+        (
+            label_list,
+            ["label_c1", "label_c2"],
+            intensity_list,
+            ["intensity_c1", "intensity_c2"],
+            {
+                "label_images": label_list,
+                "label_names": ["label_c1", "label_c2"],
+                "intensity_images": intensity_list,
+                "intensity_names": ["intensity_c1", "intensity_c2"],
+            },
+        ),
+    ],
 )
-def test_generate_measure_dict(label_images, label_names, intensity_images, intensity_names, expected):
-    result = _generate_measure_dict(label_images, label_names, intensity_images, intensity_names)
-    assert result['label_images'][0].all() == expected['label_images'][0].all()
-    assert result['label_names'] == expected['label_names']
-    assert isinstance(result['label_names'], list)
+def test_generate_measure_dict(
+    label_images, label_names, intensity_images, intensity_names, expected
+):
+    result = _generate_measure_dict(
+        label_images, label_names, intensity_images, intensity_names
+    )
+    assert result["label_images"][0].all() == expected["label_images"][0].all()
+    assert result["label_names"] == expected["label_names"]
+    assert isinstance(result["label_names"], list)
     # assert intensity images are the same, if they exist
-    if result['intensity_images'] is not None:
-        assert result['intensity_images'][0].all() == expected['intensity_images'][0].all()
-        assert result['intensity_names'] == expected['intensity_names']
+    if result["intensity_images"] is not None:
+        assert (
+            result["intensity_images"][0].all()
+            == expected["intensity_images"][0].all()
+        )
+        assert result["intensity_names"] == expected["intensity_names"]
     else:
-        assert result['intensity_images'] is None
-        assert result['intensity_names'] is None
-        
-        
+        assert result["intensity_images"] is None
+        assert result["intensity_names"] is None
+
+
 def test_extract_info_from_id_string():
     id_string = "P14-A6__2024-07-16 25x 18HIC ncoa4 FT dapi obl 01"
     id_regex = {
-        'scene': r'(P\d{1,3}-\w+)__',
-        'well': r'-(\w+)__',
-        'HIC': r'(\d{1,3})HIC',
-        'exp': r'obl (\d{2,3})'
+        "scene": r"(P\d{1,3}-\w+)__",
+        "well": r"-(\w+)__",
+        "HIC": r"(\d{1,3})HIC",
+        "exp": r"obl (\d{2,3})",
     }
-    expected = {'scene': 'P14-A6', 'well': 'A6', 'HIC': '18', 'exp': '01'}
+    expected = {"scene": "P14-A6", "well": "A6", "HIC": "18", "exp": "01"}
     result = _extract_info_from_id_string(id_string, id_regex)
     assert result == expected
 
@@ -106,11 +155,13 @@ def test_map_tx_dict_to_df_id_col():
         "Treatment2": {"Condition3": ["D4:E5"]},
     }
     tx_n_well = 96
-    target_df = pd.DataFrame({
-        'well': ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
-        'value': [10, 20, 30, 40, 50, 60]
-    })
-    id_column = 'well'
+    target_df = pd.DataFrame(
+        {
+            "well": ["A1", "A2", "A3", "A4", "A5", "A6"],
+            "value": [10, 20, 30, 40, 50, 60],
+        }
+    )
+    id_column = "well"
     result = map_tx_dict_to_df_id_col(tx, tx_n_well, target_df, id_column)
     print(result)
     assert isinstance(result, pd.DataFrame)
@@ -124,27 +175,91 @@ def test_map_tx_dict_to_df_id_col():
 
 # Define test data
 label_image_2d = np.array([[0, 0, 1], [0, 1, 1], [1, 1, 0]])
-label_image_3d = np.array([[[0, 0, 1], [0, 1, 1], [1, 1, 0]], [[0, 0, 1], [0, 1, 1], [1, 1, 0]]])
+label_image_3d = np.array(
+    [[[0, 0, 1], [0, 1, 1], [1, 1, 0]], [[0, 0, 1], [0, 1, 1], [1, 1, 0]]]
+)
 intensity_image_2d = np.array([[10, 10, 20], [10, 20, 20], [20, 20, 10]])
-intensity_image_3d = np.array([[[10, 10, 20], [10, 20, 20], [20, 20, 10]], [[10, 10, 20], [10, 20, 20], [20, 20, 10]]])
-id_string = 'P14-A6__2024-07-16 25x 18HIC ncoa4 FT dapi obl 01'
+intensity_image_3d = np.array(
+    [
+        [[10, 10, 20], [10, 20, 20], [20, 20, 10]],
+        [[10, 10, 20], [10, 20, 20], [20, 20, 10]],
+    ]
+)
+id_string = "P14-A6__2024-07-16 25x 18HIC ncoa4 FT dapi obl 01"
 id_regex = {
-    'well': '-(\w+)__',
-    'HIC': r'(\d{1,3})HIC',
-    'exp': r'obl (\d{2,3})'
+    "well": r"-(\w+)__",
+    "HIC": r"(\d{1,3})HIC",
+    "exp": r"obl (\d{2,3})",
 }
 
 
 @pytest.mark.parametrize(
-    "label_images, label_names, intensity_images, intensity_names, properties, scale, id_string, id_regex_dict, save_data_path, expected_columns",
+    "label_images, label_names, intensity_images, intensity_names, "
+    "properties, scale, id_string, id_regex_dict, "
+    "save_data_path, expected_columns",
     [
-        (label_image_2d, None, None, None, ['area'], (1.0, 1.0), id_string, id_regex, None, ['id_string', 'well', 'HIC', 'exp', 'area']),
-        ([label_image_2d], None, [intensity_image_2d], None, ['area', 'intensity_mean'], (1.0, 1.0), "test_id", None, None, ['id_string', 'area', 'intensity_mean']),
-        ([label_image_3d], None, None, None, ['area'], (1.0, 1.0, 1.0), "test_id", None, None, ['id_string', 'area']),
-        ([label_image_3d], None, [intensity_image_3d], None, ['area', 'intensity_mean'], (1.0, 1.0, 1.0), "test_id", None, None, ['id_string', 'area', 'intensity_mean']),
-    ]
+        (
+            label_image_2d,
+            None,
+            None,
+            None,
+            ["area"],
+            (1.0, 1.0),
+            id_string,
+            id_regex,
+            None,
+            ["id", "well", "HIC", "exp", "area"],
+        ),
+        (
+            [label_image_2d],
+            None,
+            [intensity_image_2d],
+            None,
+            ["area", "intensity_mean"],
+            (1.0, 1.0),
+            "test_id",
+            None,
+            None,
+            ["id", "area", "intensity_mean"],
+        ),
+        (
+            [label_image_3d],
+            None,
+            None,
+            None,
+            ["area"],
+            (1.0, 1.0, 1.0),
+            "test_id",
+            None,
+            None,
+            ["id", "area"],
+        ),
+        (
+            [label_image_3d],
+            None,
+            [intensity_image_3d],
+            None,
+            ["area", "intensity_mean"],
+            (1.0, 1.0, 1.0),
+            "test_id",
+            None,
+            None,
+            ["id", "area", "intensity_mean"],
+        ),
+    ],
 )
-def test_measure_regionprops(label_images, label_names, intensity_images, intensity_names, properties, scale, id_string, id_regex_dict, save_data_path, expected_columns):
+def test_measure_regionprops(
+    label_images,
+    label_names,
+    intensity_images,
+    intensity_names,
+    properties,
+    scale,
+    id_string,
+    id_regex_dict,
+    save_data_path,
+    expected_columns,
+):
     result_df = measure_regionprops(
         label_images=label_images,
         label_names=label_names,
@@ -156,28 +271,28 @@ def test_measure_regionprops(label_images, label_names, intensity_images, intens
         id_regex_dict=id_regex_dict,
         save_data_path=save_data_path,
     )
-    
+
     print(result_df)
-    
+
     assert isinstance(result_df, pd.DataFrame)
     # Check if the DataFrame contains the expected columns
     assert all(column in result_df.columns for column in expected_columns)
     # Check if the id_string column contains the correct value
-    assert (result_df['id_string'] == id_string).all()
-    
-    
+    assert (result_df["id"] == id_string).all()
+
+
 def test_measure_regionprops_tx_dict():
     label_images = [label_image_2d]
     intensity_images = [intensity_image_2d]
-    properties = ['area', 'intensity_mean']
+    properties = ["area", "intensity_mean"]
     scale = (1.0, 1.0)
-    id_string = 'P14-A6__2024-07-16 25x 18HIC ncoa4 FT dapi obl 01'
+    id_string = "P14-A6__2024-07-16 25x 18HIC ncoa4 FT dapi obl 01"
     id_regex_dict = {
-        'well': '-(\w+)__',
-        'HIC': r'(\d{1,3})HIC',
-        'exp': r'obl (\d{2,3})'
+        "well": r"-(\w+)__",
+        "HIC": r"(\d{1,3})HIC",
+        "exp": r"obl (\d{2,3})",
     }
-    tx_id = 'well'
+    tx_id = "well"
     tx_dict = {
         "Treatment1": {"Condition1": ["A1", "B2"], "Condition2": ["C3"]},
         "Treatment2": {"Condition3": ["D4:E5"]},
@@ -192,20 +307,41 @@ def test_measure_regionprops_tx_dict():
         id_regex_dict=id_regex_dict,
         tx_id=tx_id,
         tx_n_well=tx_n_well,
-        tx_dict=tx_dict
+        tx_dict=tx_dict,
     )
-    
+
     print(result_df)
-    
+
     assert isinstance(result_df, pd.DataFrame)
     # Check if the DataFrame contains the expected columns
-    assert all(column in result_df.columns for column in ['id_string', 'area', 'intensity_mean', 'Treatment1', 'Treatment2'])
+    assert all(
+        column in result_df.columns
+        for column in [
+            "id",
+            "area",
+            "intensity_mean",
+            "Treatment1",
+            "Treatment2",
+        ]
+    )
     # Check if the id_string column contains the correct value
-    assert (result_df['id_string'] == id_string).all()
+    assert (result_df["id"] == id_string).all()
     # Check if the treatments were assigned correctly
-    assert (result_df.loc[result_df['well'] == 'A1', 'Treatment1'] == 'Condition1').all()
-    assert (result_df.loc[result_df['well'] == 'B2', 'Treatment1'] == 'Condition1').all()
-    assert (result_df.loc[result_df['well'] == 'C3', 'Treatment1'] == 'Condition2').all()
-    assert (result_df.loc[result_df['well'] == 'D4', 'Treatment2'] == 'Condition3').all()
-    assert (result_df.loc[result_df['well'] == 'E5', 'Treatment2'] == 'Condition3').all()
-    assert (result_df.loc[result_df['well'] == 'E4', 'Treatment2'] == 'Condition3').all()
+    assert (
+        result_df.loc[result_df["well"] == "A1", "Treatment1"] == "Condition1"
+    ).all()
+    assert (
+        result_df.loc[result_df["well"] == "B2", "Treatment1"] == "Condition1"
+    ).all()
+    assert (
+        result_df.loc[result_df["well"] == "C3", "Treatment1"] == "Condition2"
+    ).all()
+    assert (
+        result_df.loc[result_df["well"] == "D4", "Treatment2"] == "Condition3"
+    ).all()
+    assert (
+        result_df.loc[result_df["well"] == "E5", "Treatment2"] == "Condition3"
+    ).all()
+    assert (
+        result_df.loc[result_df["well"] == "E4", "Treatment2"] == "Condition3"
+    ).all()
