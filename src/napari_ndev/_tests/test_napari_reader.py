94200f63116914c0c05afc16d7f1ce7be4a6ed21
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-10-11 09:48:51 -0500

    tests passing

--- src/napari_ndev/_tests/test_napari_reader.py
+++ src/napari_ndev/_tests/test_napari_reader.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 import dask.array as da
-import npe2
+
+# import npe2
 import numpy as np
 import pytest
 
@@ -15,9 +16,10 @@ if TYPE_CHECKING:
 
 ###############################################################################
 
-PNG_FILE = "example.png"
-GIF_FILE = "example.gif"
-OME_TIFF = "pipeline-4.ome.tiff"
+RGB_TIFF = "RGB.tiff" # has two scense
+# PNG_FILE = "example.png"
+# GIF_FILE = "example.gif"
+# OME_TIFF = "pipeline-4.ome.tiff"
 
 ###############################################################################
 
@@ -31,27 +33,21 @@ OME_TIFF = "pipeline-4.ome.tiff"
 @pytest.mark.parametrize(
     ("filename", "expected_shape", "expected_meta"),
     [
-        # (PNG_FILE, (800, 537, 4), {"name": Path(PNG_FILE).stem, "rgb": True}),
-        # (GIF_FILE, (72, 268, 268, 3), {"name": Path(GIF_FILE).stem, "rgb": True}),
         (
-            OME_TIFF,
-            (4, 65, 600, 900),
+            RGB_TIFF,
+            (1440, 1920, 3),
             {
-                "name": [
-                    f"{Path(OME_TIFF).stem} :: Bright_2",
-                    f"{Path(OME_TIFF).stem} :: EGFP",
-                    f"{Path(OME_TIFF).stem} :: CMDRP",
-                    f"{Path(OME_TIFF).stem} :: H3342",
-                ],
-                "channel_axis": 0,
-                "scale": (0.29, 0.10833333333333332, 0.10833333333333332),
-            },
+                'name': 'Image:0',
+                'scale': (264.5833333333333, 264.5833333333333),
+                'rgb': True,
+            }
         ),
     ],
 )
 def test_reader(
     resources_dir: Path,
     filename: str,
+    in_memory: bool,
     expected_shape: tuple[int, ...],
     expected_dtype: type,
     expected_meta: dict[str, Any],
@@ -62,7 +58,7 @@ def test_reader(
         path = str(resources_dir / filename)
 
     # Get reader
-    reader = napari_get_reader(path)
+    reader = napari_get_reader(path, in_memory=in_memory, open_first_scene_only=True)
 
     # Check callable
     assert callable(reader)
@@ -72,17 +68,17 @@ def test_reader(
 
     # We only return one layer
     if layer_data is not None:
-        data, meta, _ = layer_data[0]  # type: ignore
+        data, meta, _ = layer_data[0]
 
         # Check layer data
-        assert isinstance(data, expected_dtype)  # type: ignore
-        assert data.shape == expected_shape  # type: ignore
+        assert isinstance(data, expected_dtype)
+        assert data.shape == expected_shape
 
         # Check meta
-        meta.pop("metadata", None)
-        assert meta == expected_meta  # type: ignore
+        meta.pop('metadata', None)
+        assert meta == expected_meta
 
-        # confirm that this also works via npe2
-        with npe2pm.tmp_plugin(package="napari-ndev") as plugin:
-            [via_npe2] = npe2.read([path], stack=False, plugin_name=plugin.name)
-            assert via_npe2[0].shape == data.shape  # type: ignore
+        # # confirm that this also works via npe2
+        # with npe2pm.tmp_plugin(package='napari-ndev') as plugin:
+        #     [via_npe2] = npe2.read([path], stack=False, plugin_name=plugin.name)
+        #     assert via_npe2[0].shape == data.shape
