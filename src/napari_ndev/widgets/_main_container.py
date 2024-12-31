a5446f9f8ee836379b9a6db31d2db4f375e6aa0a
TimMonko <47310455+TimMonko@users.noreply.github.com>
2024-12-31 09:58:15 -0600

    layout with links

--- src/napari_ndev/widgets/_main_container.py
+++ src/napari_ndev/widgets/_main_container.py
@@ -7,9 +7,12 @@ from magicclass.widgets import (
     TabbedContainer,
 )
 from magicgui.widgets import (
+    Container,
     Label,
-    Image,
+    PushButton,
 )
+
+from napari_ndev import __version__
 
 if TYPE_CHECKING:
     import napari
@@ -41,21 +44,34 @@ class MainContainer(ScrollableContainer):
         self._viewer = viewer if viewer is not None else None
 
         # TODO: get image to display. Works outside of napari
-        self._logo = Image(value=r"docs\resources\images\ndev-logo.png")
-        self._logo.scale_widget_to_image_size()
-
-        self._link_label = Label(
-            value=(
-                '<p style="color: white;">' # doesn't appear to do anything
-                '<a href="https://timmonko.github.io/napari-ndev"'
-                'style="color: white;">'
-                'nDev documentation</a></p>'
-            )
+        # self._logo = Image(value=r'docs\resources\images\ndev-logo.png')
+        # self._logo.scale_widget_to_image_size()
+        # _logo_path = r'docs\resources\images\ndev-logo.png'
+        _logo_path = r'docs\resources\images\neuralDev-logo.svg'
+        self._logo_label = Label(
+            value='<h1 style="text-align: center;">'
+            f'<img src="{_logo_path}" style="width: 20%; max-width: 50px;">'
+            '</h1>'
         )
-        self._link_label.native.setOpenExternalLinks(True)
+        self._version_label = Label(value=f'v{__version__}')
+
+
+        self._docs_link_button = PushButton(
+            text='Docs',
+            icon='ic:round-menu-book'
+        )
+        self._bug_report_link_button = PushButton(
+            text='Bug Report',
+            icon='ic:outline-bug-report',
+        )
+        self._link_container = Container(
+            widgets=[self._docs_link_button, self._bug_report_link_button],
+            layout='horizontal',
+        )
 
         self._init_widget_containers()
         self._init_layout()
+        self._init_callbacks()
 
     def _init_widget_containers(self):
         from napari_ndev import (
@@ -88,7 +104,29 @@ class MainContainer(ScrollableContainer):
 
     def _init_layout(self):
         """Initialize the layout."""
-        self.append(self._logo)
-        self.append(self._link_label)
+        self.append(self._logo_label)
+        self.append(self._version_label)
+        self.append(self._link_container)
         self.append(self._tabbed_container)
         # self.stretch(self._tabbed_container)
+
+    def _init_callbacks(self):
+        """Initialize the widget callbacks."""
+        self._docs_link_button.clicked.connect(self._open_docs_link)
+        self._bug_report_link_button.clicked.connect(self._open_bug_report_link)
+
+    def _open_docs_link(self):
+        """Open the documentation link."""
+        import webbrowser
+        webbrowser.open('https://timmonko.github.io/napari-ndev')
+
+    def _open_bug_report_link(self):
+        """Open the bug report link."""
+        import webbrowser
+        webbrowser.open('https://github.com/TimMonko/napari-ndev/issues')
+
+if __name__ == "__main__":
+    import napari
+    viewer = napari.Viewer()
+    viewer.window.add_dock_widget(MainContainer(viewer=viewer))
+    napari.run()
