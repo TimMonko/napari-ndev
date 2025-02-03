6cba38c0638a637fd8fb45400f7f0aa6279bf3fc
TimMonko <47310455+TimMonko@users.noreply.github.com>
2025-02-03 14:00:21 -0600

    add ability to export figures and screenshots

--- src/napari_ndev/widgets/_settings_container.py
+++ src/napari_ndev/widgets/_settings_container.py
@@ -5,14 +5,21 @@ if sys.version_info >= (3, 10):
 else:
     from importlib_metadata import entry_points
 
-from magicgui.widgets import CheckBox, ComboBox, Container
+from magicclass.widgets import GroupBoxContainer
+from magicgui.widgets import (
+    CheckBox,
+    ComboBox,
+    Container,
+    FloatSpinBox,
+    TupleEdit,
+)
 
 from napari_ndev._settings import get_settings
 
 
 class SettingsContainer(Container):
     def __init__(self):
-        super().__init__()
+        super().__init__(labels=False)
         self.settings = get_settings()
         self._available_readers = [
             reader.name for reader in entry_points(group='bioio.readers') # use entry_points(group='bioio.readers') for py >= 3.10
@@ -58,7 +65,8 @@ class SettingsContainer(Container):
             label='Clear All Layers On New Scene Selection',
             tooltip='Whether to clear the viewer when selecting a new scene.',
         )
-        self._bioio_settings_container = Container(
+        self._bioio_settings_container = GroupBoxContainer(
+            name='Reader Settings',
             widgets=[
                 self._preferred_reader_combo,
                 self._scene_handling_combo,
@@ -68,8 +76,40 @@ class SettingsContainer(Container):
             layout='vertical',
         )
 
+        self._canvas_scale_slider = FloatSpinBox(
+            label='Canvas Scale',
+            value=self.settings.CANVAS_SCALE,
+            min=0.01,
+            max=100.0,
+            step=1.0,
+            tooltip='Scales exported figures and screenshots by this value.',
+        )
+        self._override_canvas_size_checkbox = CheckBox(
+            value=self.settings.OVERRIDE_CANVAS_SIZE,
+            label='Override Canvas Size',
+            tooltip='Whether to override the canvas size when exporting canvas screenshot.',
+        )
+        self._canvas_size_tuple = TupleEdit(
+            value = self.settings.CANVAS_SIZE,
+            label='Canvas Size',
+            tooltip='Height x width of the canvas when exporting a screenshot.'
+            ' Only used if "Override Canvas Size" is checked.',
+        )
+        self._export_settings_container = GroupBoxContainer(
+            name='Export Settings',
+            widgets=[
+                self._canvas_scale_slider,
+                self._override_canvas_size_checkbox,
+                self._canvas_size_tuple,
+            ],
+            layout='vertical',
+        )
+
+
+
         self.extend([
             self._bioio_settings_container,
+            self._export_settings_container
         ])
         self.native.layout().addStretch()
 
@@ -78,6 +118,9 @@ class SettingsContainer(Container):
         self._scene_handling_combo.changed.connect(self._update_settings)
         self._clear_on_scene_select_checkbox.changed.connect(self._update_settings)
         self._unpack_channels_as_layers_checkbox.changed.connect(self._update_settings)
+        self._canvas_scale_slider.changed.connect(self._update_settings)
+        self._override_canvas_size_checkbox.changed.connect(self._update_settings)
+        self._canvas_size_tuple.changed.connect(self._update_settings)
 
     def _update_settings(self):
         self._preferred_reader = self._preferred_reader_combo.value
@@ -86,8 +129,12 @@ class SettingsContainer(Container):
             'SCENE_HANDLING': self._scene_handling_combo,
             'CLEAR_LAYERS_ON_NEW_SCENE': self._clear_on_scene_select_checkbox,
             'UNPACK_CHANNELS_AS_LAYERS': self._unpack_channels_as_layers_checkbox,
+            'CANVAS_SCALE': self._canvas_scale_slider,
+            'OVERRIDE_CANVAS_SIZE': self._override_canvas_size_checkbox,
+            'CANVAS_SIZE': self._canvas_size_tuple,
         }
 
+        # the below updates all the settings of the singleton after something changes
         for setting_name, widget in widget_to_setting.items():
             setattr(self.settings, setting_name, widget.value)
 
